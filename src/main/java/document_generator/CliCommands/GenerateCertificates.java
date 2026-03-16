772d360f7a1d3fec8e55d9916bfe9c634ca7e677
Daniel Puchala <97415136+xpuchal1@users.noreply.github.com>
2026-03-16 15:52:24 +0100

    fix: Generate certificates

--- src/main/java/document_generator/CliCommands/GenerateCertificates.java
+++ src/main/java/document_generator/CliCommands/GenerateCertificates.java
@@ -13,13 +13,13 @@ public class GenerateCertificates implements Runnable {
         try {
             CertificateBundle ca = Certificates.generateCertificate("EU", "Certificate Authority",
                 null, null, true, null);
-            Certificates.exportKey(ca.getKey(), Path.of("src/main/resources/keys/ca.getKey()"));
+            Certificates.exportKey(ca.getKey(), Path.of("src/main/resources/keys/ca.key"));
             Certificates.exportCert(ca.getCert(), Path.of("src/main/resources/certificates/ca.pem"));
 
             // Create key, cert for Trusted Party server
             CertificateBundle tp = Certificates.generateCertificate("CZ", "Trusted Party",
                 ca.getKey(), ca.getCert(), true, 0);
-            Certificates.exportKey(tp.getKey(), Path.of("src/main/resources/keys/tp.getKey()"));
+            Certificates.exportKey(tp.getKey(), Path.of("src/main/resources/keys/tp.key"));
             Certificates.exportCert(tp.getCert(), Path.of("src/main/resources/certificates/tp.pem"));
 
             // Create key, cert for 2 intermediates
@@ -28,15 +28,10 @@ public class GenerateCertificates implements Runnable {
             CertificateBundle int2 = Certificates.generateCertificate("CZ", "Intermediate Two",
                 int1.getKey(), int1.getCert(), true, 1);
 
-            Certificates.exportKey(int1.getKey(), Path.of("src/main/resources/keys/int1.getKey()"));
-            Certificates.exportKey(int2.getKey(), Path.of("src/main/resources/keys/int2.getKey()"));
+            Certificates.exportKey(int1.getKey(), Path.of("src/main/resources/keys/int1.key"));
+            Certificates.exportKey(int2.getKey(), Path.of("src/main/resources/keys/int2.key"));
             Certificates.exportCert(int1.getCert(), Path.of("src/main/resources/certificates/int1.pem"));
             Certificates.exportCert(int2.getCert(), Path.of("src/main/resources/certificates/int2.pem"));
-
-            CertificateBundle organizationKey = Certificates.generateCertificate("EU", "XXXXXXXX",
-                int2.getKey(), int2.getCert(), false, null);
-            Certificates.exportKey(organizationKey.getKey(), Path.of("src/main/resources/keys/xxxxxxxx.getKey()"));
-            Certificates.exportCert(organizationKey.getCert(), Path.of("src/main/resources/certificates/xxxxxxxx.pem"));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
